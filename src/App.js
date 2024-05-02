02ea2fbc055bbffa83ef92670a4a300688a89cea
patel9619ali <patel9619ali@gmail.com>
2024-05-02 18:30:41 +0530

    steps-final

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 
+import { useState, Fragment } from 'react';
 import './index.css';
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
-function handlePrevious(){
-  alert("Previous");
-}
-function handleNext(){
-  alert("Next");
-}
+const personName = [
+  {name:'Ali'},
+  {name:'Kaif'},
+  {name:'Saif'},
+];
 function App() {
-  const steps = 3;
+  let [steps,setSteps] = useState(1);
+  let [previoussteps,setPrevioussteps] = useState(3)
+  let [isOpen,setisOpen] = useState(true)
+  // console.log(previoussteps)
+  function handlePrevious(){
+    if(steps > 1) {
+      setSteps((currentSteps)=>currentSteps - 1);
+      setPrevioussteps(previoussteps+1);
+    }
+
+  }
+  function handleNext(){
+    if(steps < 3){
+      setSteps((currentSteps)=>currentSteps + 1);
+      setPrevioussteps(previoussteps-1);
+    }
+  }
+  function handleClose(){
+    setisOpen(!isOpen)
+  }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className='active'>1</div>
-        <div>2</div>
-        <div>3</div>
-      </div>
-      <p className='message'>{
-       messages[steps-1]
-      }</p>
-      <div className="buttons">
-        <button style={{color: '#fff', backgroundColor: "Purple"}} onClick={handlePrevious}>Previous</button>
-        <button style={{color: '#fff', backgroundColor: "Purple"}} onClick={handleNext}>Next</button>
-      </div>
-    </div>
+    <Fragment>
+      {/* <div className="button_wrapper"> */}
+      <button className='close_button' onClick={handleClose}>{isOpen ? "Close" : "Open"}</button>
+      {/* </div> */}
+      {isOpen && <div className="steps">
+        <div className="numbers">
+          <div className={steps >= 1 ? "active" : ""}>1</div>
+          <div className={steps >= 2 ? "active" : ""}>2</div>
+          <div className={steps >= 3 ? "active" : ""}>3</div>
+        </div>
+        <p className='message'>{
+        messages[steps-1]
+        }</p>
+        <p className='message'>{
+        personName[previoussteps-1].name
+        }</p>
+        <div className="buttons">
+          <button style={{color: '#fff', backgroundColor: "Purple"}} onClick={handlePrevious}>Previous</button>
+          <button style={{color: '#fff', backgroundColor: "Purple"}} onClick={handleNext}>Next</button>
+        </div>
+      </div>}
+    </Fragment>
+
   );
 }
 
